b762389459e114c781d3b5176a7c39ebc328edf0
Moonwalkert3ch <6620@holbertonstudents.com>
2024-09-10 14:13:13 -0500

    adjust size of cover art

--- src/components/CoverArt.jsx
+++ src/components/CoverArt.jsx
@@ -3,9 +3,8 @@ import coverArt from '../assets/placeholder.svg';
 
 export function CoverArt() {
     return (
-      <div className="flex flex-col items-center">
-        <img src={coverArt} alt="Cover Art" className="w-48 h-48 object-cover" />
-        <p className="mt-4 text-red-500">This is a red text under the cover art</p>
+      <div className="flex flex-col">
+        <img src={coverArt} alt="Cover Art" className="w-96 h-96 object-cover" />
       </div>
     );
   }
