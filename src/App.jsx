3d0dc3b82bbb4861a2504dd8485a813106f0c6f4
Moonwalkert3ch <6620@holbertonstudents.com>
2024-09-10 16:09:35 -0500

    the play buttons are styled

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Footer from "./components/Footer";
 import MusicPlayer from "./MusicPlayer";
 import { CoverArt } from "./components/CoverArt";
 import { SongTitle } from "./components/SongTitle";
+import { PlayControls } from "./components/PlayControls";
 
 function App() {
   return (
     <div className="h-full flex flex-col justify-between p-8 min-h-screen">
       <CoverArt />
       <SongTitle />
+      <PlayControls />
       <MusicPlayer />
       <Footer />
     </div>
