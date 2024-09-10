d790c23ab4531a7f5e43245dff5a49c9c3db34d3
Moonwalkert3ch <6620@holbertonstudents.com>
2024-09-10 16:04:58 -0500

    styled song title component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Footer from "./components/Footer";
 import MusicPlayer from "./MusicPlayer";
 import { CoverArt } from "./components/CoverArt";
+import { SongTitle } from "./components/SongTitle";
 
 function App() {
   return (
     <div className="h-full flex flex-col justify-between p-8 min-h-screen">
       <CoverArt />
+      <SongTitle />
       <MusicPlayer />
       <Footer />
     </div>
